be2eda706aedefd086bfa5df7d9e8df9633e402c
sakthivel155 <sakthivelramesh155@gmail.com>
2024-08-03 09:00:28 +0530

    mobile response search and menu navbar

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ function focusInput() {
     let searchInput = document.getElementById('search-input');
     searchInput.focus();
 }
+const menu = document.querySelector('.bx-menu');
+const searchBox = document.querySelector('.bx-search');
+const mobileNav = document.querySelector('.mobile-nav');
+const searchClose = document.querySelector('.search-close');
+
+
+
+menu.addEventListener('click', () => {
+    mobileNav.style.display = mobileNav.style.display === 'none' ? 'block' : 'none';
+});
+
+searchBox.addEventListener('click', () => {
+    document.querySelector('.navbar-container').style.display =
+        document.querySelector('.navbar-container').style.display === 'block' ? 'none' : 'block';
+    document.querySelector('.mobile-searchBox').style.display = 
+        document.querySelector('.mobile-searchBox').style.display === 'none' ? 'flex' : 'none';
+});
+searchClose.addEventListener('click', () => {
+    document.querySelector('.mobile-searchBox').style.display = 
+        document.querySelector('.mobile-searchBox').style.display === 'none' ? 'flex' : 'none';
+        document.querySelector('.navbar-container').style.display =
+        document.querySelector('.navbar-container').style.display === 'block' ? 'none' : 'block';
+});
